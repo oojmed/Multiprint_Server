36a1dee90f8496cacfafcbdc4d9177b16212a630
Oj <oj@oojmed.com>
2020-01-11 18:28:32 +0000

    Make index / homepage more responsive and work on all sized screens

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     </head>
     
     <body style="margin: 0; width: 100%; height: 100%;">
-        <center><font style='position: absolute; top: 50%; left: 50%; letter-spacing: 20px; text-shadow: 0px 0px 10px #1f1f1f; transform: translate(-50%, -50%); font-family: Comic Sans, Comic Sans MS; font-size: 300px;'><font color='#ff1a1a'>t</font><font color='#ff5b1a'>h</font><font color='#ff911a'>i</font><font color='#ffc71a'>n</font><font color='#f7ff1a'>k</font><font color='#b6ff1a'>i</font><font color='#70ff1a'>n</font><font color='#1aff70'>g</font><font color='#1affb1'></font></font></center>
+        <font style='position: absolute; top: 50%; left: 50%; letter-spacing: 1vw; text-shadow: 0px 0px 10px #1f1f1f; transform: translate(-50%, -50%); font-family: Comic Sans, Comic Sans MS; font-size: 22vw;'><font color='#ff1a1a'>t</font><font color='#ff5b1a'>h</font><font color='#ff911a'>i</font><font color='#ffc71a'>n</font><font color='#f7ff1a'>k</font><font color='#b6ff1a'>i</font><font color='#70ff1a'>n</font><font color='#1aff70'>g</font><font color='#1affb1'></font></font>
         <img style="width: 100%; height: 100%;" src="https://discordapp.com/assets/263a7f4eeb6f69e46d969fa479188592.svg">
     </body>
 </html>
