187234d32d3cce79100a3d32cbcd21f2bc59b487
Oj <oj@oojmed.com>
2020-01-11 16:09:47 +0000

    Revamp / redo

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://choosealicense.com/licenses/mit/l)
 
 Multiprint utilities / scripts to be hosted on a server.
-
-Produced in collaboration with [Kognise](https://github.com/Kognise).
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Thinking</title>
+        <link rel='icon' href='https://discordapp.com/assets/263a7f4eeb6f69e46d969fa479188592.svg' type='image/svg+xml'/ >
+        <style>
+            div {
+                opacity: 0;
+            }
+        </style>
+    </head>
+    
+    <body style="margin: 0; width: 100%; height: 100%;">
+        <center><font style='position: absolute; top: 50%; left: 50%; letter-spacing: 20px; text-shadow: 0px 0px 10px #1f1f1f; transform: translate(-50%, -50%); font-family: Comic Sans, Comic Sans MS; font-size: 300px;'><font color='#ff1a1a'>t</font><font color='#ff5b1a'>h</font><font color='#ff911a'>i</font><font color='#ffc71a'>n</font><font color='#f7ff1a'>k</font><font color='#b6ff1a'>i</font><font color='#70ff1a'>n</font><font color='#1aff70'>g</font><font color='#1affb1'></font></font></center>
+        <img style="width: 100%; height: 100%;" src="https://discordapp.com/assets/263a7f4eeb6f69e46d969fa479188592.svg">
+    </body>
+</html>
